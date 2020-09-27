27f95373780959734c136284f411e03afe8c38b6
Tyler W <twildrick@usf.edu>
2020-09-27 02:31:38 -0400

    Final edit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿/*
     Author: Tyler Wildrick
     Date: 09/26/2020
-    Comments: This C# Console application code demonstrates the use of
-              Arrays after getting input from users. 
+    Comments: This is TechProject3B which is a  C# Console application code that demonstrates the use of
+              of a mnaual Array. 
 */
 
 using System;
@@ -12,7 +12,7 @@ namespace TechProject3B
     class Program
     {
         static void Main(string[] args)
-        {            
+        {
             // Here is a manually populated array
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
 
@@ -26,7 +26,7 @@ namespace TechProject3B
             Console.WriteLine("__________________________________________________________________________");
             Console.WriteLine("Press any key to exit the program ...");
             // Pause the program and await the user to press a key to end the program
-            Console.ReadKey(true);             
+            Console.ReadKey(true); 
         }
     }
 }
